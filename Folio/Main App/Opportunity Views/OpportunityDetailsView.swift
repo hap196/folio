75a9d53cd338f254bb72fe1f7b8ac69caf6ef67a
Hailey Pan <haileyrpan@gmail.com>
2024-02-04 06:42:41 -0500

    opportunity details view fixes

--- Folio/Main App/Opportunity Views/OpportunityDetailsView.swift
+++ Folio/Main App/Opportunity Views/OpportunityDetailsView.swift
@@ -9,68 +9,116 @@ import SwiftUI
 
 struct OpportunityDetailsView: View {
     var opportunity: Opportunity
-
+    
+    @State private var showingSafariView = false
+    
+    private var applyLink: URL? {
+        URL(string: opportunity.link)
+    }
+    
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 10) {
-                Text(opportunity.title)
-                    .font(.title2)
-                    .fontWeight(.bold)
-
-                Text(opportunity.organization)
-                    .font(.title3)
-                    .foregroundColor(.secondary)
-
-                Text(opportunity.location)
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-
-                Divider()
-
-                // Tab View for Description, Company, Review, etc.
-                TabView {
-                    Text(opportunity.description)
-                        .tabItem {
-                            Text("Description")
-                        }
+        ZStack {
+            // Main ScrollView Content
+            ScrollView {
+                VStack(alignment: .leading, spacing: 10) {
                     
-                    Text("Company information here.") // Replace with actual company information
-                        .tabItem {
-                            Text("Company")
-                        }
+                    Text(opportunity.title)
+                        .font(.title2)
+                        .fontWeight(.bold)
                     
-                    Text("Reviews here.") // Replace with actual reviews
-                        .tabItem {
-                            Text("Review")
-                        }
-                }
+                    HStack {
+                        Image("placeholder") // Placeholder image
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .cornerRadius(8)
 
-                // Qualification Section
-                VStack(alignment: .leading) {
-                    Text("Qualifications")
+                        VStack(alignment: .leading) {
+
+                            HStack {
+                                Image(systemName: "location.fill") // Location icon
+                                Text(opportunity.location)
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                            }
+
+                            // Tags for type and level
+                            HStack {
+                                Text(opportunity.type)
+                                    .font(.caption)
+                                    .padding(5)
+                                    .background(Color.gray.opacity(0.2))
+                                    .cornerRadius(5)
+                                
+                                Text(opportunity.level ?? "Unknown")
+                                    .font(.caption)
+                                    .padding(5)
+                                    .background(Color.gray.opacity(0.2))
+                                    .cornerRadius(5)
+                            }
+                        }
+                    }
+
+                    Divider()
+
+                    Text("About this Opportunity")
                         .font(.headline)
+                        .fontWeight(.bold)
                         .padding(.vertical, 5)
 
-                    // List qualifications
+                    Text(opportunity.description)
+                        .font(.body)
+                        .foregroundColor(.gray)
+
+                    Text("Eligibility")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .padding(.vertical, 5)
+
                     ForEach(opportunity.eligibility?.components(separatedBy: ",") ?? [], id: \.self) { qualification in
                         Text("• \(qualification.trimmingCharacters(in: .whitespacesAndNewlines))")
                     }
+                    
+                    Spacer() // This will push all the content to the top
                 }
-
-                // Apply Now Button
-                Button(action: {
-                    // Action for Apply Now
-                }) {
-                    Text("Apply Now")
-                        .frame(maxWidth: .infinity, minHeight: 50)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                .padding(.vertical)
+                .padding()
             }
-            .padding()
+
+            // Apply Now Button at the Bottom
+            VStack {
+                Spacer() // Pushes the button to the bottom
+
+                Divider()
+
+                HStack {
+                    Button(action: {
+                                self.showingSafariView = true
+                            }) {
+                                Text("Apply Now")
+                                    .frame(maxWidth: .infinity, minHeight: 50)
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                            }
+                            .padding()
+                            .sheet(isPresented: $showingSafariView) {
+                                if let applyLink = applyLink {
+                                    SafariView(url: applyLink)
+                                }
+                            }
+
+                    Button(action: {
+                        // Action for Save
+                    }) {
+                        Image(systemName: "bookmark")
+                            .padding()
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(8)
+                    }
+                    .padding(.trailing)
+                }
+            }
         }
-        .navigationBarTitle(Text(opportunity.title), displayMode: .inline)
+        .navigationBarTitle(Text("Opportunity Details"), displayMode: .inline)
+        .edgesIgnoringSafeArea(.bottom) // Ensures the button is aligned at the very bottom
     }
 }
---
+++ Folio/Main App/Opportunity Views/SafariView.swift
@@ -0,0 +1,20 @@
+//
+//  SafariView.swift
+//  Folio
+//
+//  Created by Hailey Pan on 2/4/24.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+    }
+}
